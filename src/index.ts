188ca04a53f9602a5ad3c7745e1ddc4225766bdb
ilij4 <ilijammilosavljevic@gmail.com>
2023-05-05 15:20:26 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
-import SpheronComputeClient, {
-  Cluster,
-  ClusterFundsUsage,
-  ClusterInstanceOrder,
-  ClusterInstancesInfo,
-  MarketplaceApp,
-  ComputeMachine,
-  CreateClusterInstanceFromMarketplaceRequest,
-  ClusterInstanceStateEnum,
-  ExtendedClusterInstance,
-  CreateClusterInstanceRequest,
-  ClusterProtocolEnum,
-  ProviderEnum,
-  ClusterInstanceResponse,
-  ClusterInstance,
-  Domain,
-  InstanceLogType,
-  UpdateClusterInstaceRequest,
-  ClusterInstanceFromMarketplaceResponse,
-  DomainTypeEnum,
-} from "@spheron/compute";
+import SpheronComputeClient, { Cluster, ClusterFundsUsage, ClusterInstance, ClusterInstanceExtendedInfo, ClusterInstanceOrder, ClusterInstanceStateEnum, ClusterInstancesInfo, ClusterProtocolEnum, ComputeMachine, CreateInstanceFromMarketplaceRequest, CreateInstanceRequest, Domain, DomainTypeEnum, InstanceLogType, InstanceResponse, MarketplaceApp, MarketplaceInstanceResponse, ProviderEnum, UpdateInstaceRequest } from "@spheron/compute";
 import Logger from "./config/logger";
+import { link } from "fs";
+import { type } from "os";
 
 (async () => {
   const computeClient: SpheronComputeClient = new SpheronComputeClient({
@@ -74,184 +56,181 @@ import Logger from "./config/logger";
   //   Logger.info("asd");
   // }, 10000);
 
-  // const categories: string[] =
-  //   await computeClient.clusterTemplate.getClusterCategories();
+  const categories: string[] =
+    await computeClient.computeMarketplace.getCategories();
 
-  // const { clusterTemplates }: { clusterTemplates: ClusterTemplate[] } =
-  //   await computeClient.clusterTemplate.getClusterTemplates();
+  const marketplaceApps: MarketplaceApp[] =
+    await computeClient.computeMarketplace.getAll();
 
-  // const clusterTemplate: ClusterTemplate =
-  //   await computeClient.clusterTemplate.getClusterTemplate(
-  //     clusterTemplates[0]._id
-  //   );
+  const marketplaceApp: MarketplaceApp =
+    await computeClient.computeMarketplace.get(
+      marketplaceApps[0].id
+    );
 
-  // computeClient.cluster;
 
-  // const promises: any[] = [];
-  // const clusterInstances: ExtendedClusterInstance[] =
-  //   await computeClient.cluster.getInstances(clusters[0]._id, {
-  //     skip: 0,
-  //     limit: 10,
-  //   });
-  // clusterInstances.forEach((instance) => {
-  //   if (instance.state === ClusterInstanceStateEnum.ACTIVE)
-  //     promises.push(computeClient.clusterInstance.close(instance._id));
-  // });
+  const clusters: Cluster[] = await computeClient.organization.getClusters("63612a842e50ee3ffbebcf06", {skip:0, limit:10});
 
-  // await Promise.all(promises);
+  const clusterInstancesExtended: ClusterInstanceExtendedInfo[] =
+    await computeClient.cluster.getInstances(clusters[0].id, {
+      skip: 0,
+      limit: 10,
+    });
 
-  // const cluster: Cluster = await computeClient.cluster.getCluster(
-  //   clusters[0]._id
-  // );
+  const clusterFunds: ClusterFundsUsage =
+    await computeClient.cluster.getUsage(clusters[0].id);
 
-  // const clusterInstances: ExtendedClusterInstance[] =
-  //   await computeClient.cluster.getInstances(clusters[0]._id, {
-  //     skip: 0,
-  //     limit: 10,
-  //   });
+  const clusterInstancesInfo: ClusterInstancesInfo =
+    await computeClient.cluster.getInstancesInfo(clusters[0].id);
 
-  // const clusterFunds: ClusterFundsUsage =
-  //   await computeClient.cluster.getClusterFundsUsage(clusters[0]._id);
+  await computeClient.cluster.delete(clusters[0].id);
 
-  // const clusterInstancesInfo: ClusterInstancesInfo =
-  //   await computeClient.cluster.getClusterInstancesDetails(clusters[0]._id);
+  const computeMachines: ComputeMachine[] =
+    await computeClient.clusterMachine.get({
+      skip: 0,
+      limit: 5,
+    });
 
-  // await computeClient.cluster.deleteCluster(clusters[0]._id);
+  const regions: string[] =
+    await computeClient.clusterMachine.getRegions();
 
-  // const computeMachines: ComputeMachine[] =
-  //   await computeClient.clusterMachine.getComputeMachines({
-  //     skip: 0,
-  //     limit: 5,
-  //   });
+  const mongoTemplate: MarketplaceApp = marketplaceApps[0];
 
-  // const regions: string[] =
-  //   await computeClient.clusterMachine.getComputeMachineRegions();
+  const clusterInstanceFromTemplate: CreateInstanceFromMarketplaceRequest =
+    {
+      templateId: mongoTemplate.id,
+      environmentVariables: mongoTemplate.serviceData.variables.map(
+        (templateVar) => {
+          return {
+            label: templateVar.label,
+            value: templateVar.defaultValue,
+            isSecret: false,
+          };
+        }
+      ),
+      organizationId: "63612a842e50ee3ffbebcf06",
+      akashImageId: computeMachines[2].id,
+      region: "any",
+    };
 
-  // const mongoTemplate: ClusterTemplate = clusterTemplates[0];
+  const marketplaceInstance: MarketplaceInstanceResponse =
+    await computeClient.instance.createFromMartketplace(
+      clusterInstanceFromTemplate
+    );
 
-  // const clusterInstanceFromTemplate: CreateClusterInstanceFromTemplateRequest =
-  //   {
-  //     templateId: mongoTemplate._id,
-  //     environmentVariables: mongoTemplate.serviceData.variables.map(
-  //       (templateVar) => {
-  //         return {
-  //           label: templateVar.label,
-  //           value: templateVar.defaultValue,
-  //           isSecret: false,
-  //         };
-  //       }
-  //     ),
-  //     organizationId: "63612a842e50ee3ffbebcf06",
-  //     akashImageId: computeMachines[2]._id,
-  //     region: "any",
-  //   };
+  const updateInstance: UpdateInstaceRequest = {
+    env: [{ value: "t2=t2", isSecret: false }],
+    command: [],
+    args: [],
+    uniqueTopicId: "89fcf863-a3ab-44f2-9276-9ea75631498c",
+    tag: "latest",
+  };
 
-  // const instanceFromTemplate: ClusterInstanceFromTemplateResponse =
-  //   await computeClient.clusterInstance.createClusterInstanceFromTemplate(
-  //     clusterInstanceFromTemplate
-  //   );
+  const updateResponse: InstanceResponse =
+    await computeClient.instance.update(
+      "6448013960d4d90012b77a2a",
+      "63612a842e50ee3ffbebcf06",
+      updateInstance
+    );
 
-  // const updateInstance: UpdateClusterInstaceRequest = {
-  //   env: [{ value: "t2=t2", isSecret: false }],
-  //   command: [],
-  //   args: [],
-  //   uniqueTopicId: "89fcf863-a3ab-44f2-9276-9ea75631498c",
-  //   tag: "latest",
-  // };
+  const createClusterInstance: CreateInstanceRequest = {
+    organizationId: "63612a842e50ee3ffbebcf06",
+    configuration: {
+      folderName: "",
+      protocol: ClusterProtocolEnum.AKASH,
+      image: "crccheck/hello-world",
+      tag: "latest",
+      instanceCount: 1,
+      buildImage: false,
+      ports: [{ containerPort: 8000, exposedPort: 8000 }],
+      env: [{ value: "t=t", isSecret: false }],
+      command: [],
+      args: [],
+      region: "any",
+      akashMachineImageName: "Ventus Medium",
+    },
+    // uniqueTopicId: topicId,
+    clusterUrl: "crccheck/hello-world",
+    clusterProvider: ProviderEnum.DOCKERHUB,
+    clusterName: "wallet test",
+    healthCheckUrl: "/",
+    healthCheckPort: 8000,
+  };
 
-  // const updateResponse: ClusterInstanceResponse =
-  //   await computeClient.clusterInstance.updateClusterInstance(
-  //     "6448013960d4d90012b77a2a",
-  //     "63612a842e50ee3ffbebcf06",
-  //     updateInstance
-  //   );
-
-  // const createClusterInstance: CreateClusterInstanceRequest = {
-  //   organizationId: "63612a842e50ee3ffbebcf06",
-  //   configuration: {
-  //     folderName: "",
-  //     protocol: ClusterProtocolEnum.AKASH,
-  //     image: "crccheck/hello-world",
-  //     tag: "latest",
-  //     instanceCount: 1,
-  //     buildImage: false,
-  //     ports: [{ containerPort: 8000, exposedPort: 8000 }],
-  //     env: [{ value: "t=t", isSecret: false }],
-  //     command: [],
-  //     args: [],
-  //     region: "any",
-  //     akashMachineImageName: "Ventus Medium",
-  //   },
-  //   uniqueTopicId: topicId,
-  //   clusterUrl: "crccheck/hello-world",
-  //   clusterProvider: ProviderEnum.DOCKERHUB,
-  //   clusterName: "wallet test",
-  //   healthCheckUrl: "/",
-  //   healthCheckPort: 8000,
-  // };
-
-  // const instanceFromTemplate: ClusterInstanceResponse =
-  //   await computeClient.clusterInstance.create(createClusterInstance);
+  const instanceFromTemplate: InstanceResponse =
+    await computeClient.instance.create(createClusterInstance);
 
   // const numberOfInstances = 1;
 
   // const promises: any[] = [];
   // for (let i = 0; i < numberOfInstances; i++) {
-  //   promises.push(computeClient.clusterInstance.create(createClusterInstance));
+  //   promises.push(computeClient.instance.create(createClusterInstance));
   // }
 
+  // clusterInstancesExtended.forEach((instance) => {
+  //   if (instance.state === ClusterInstanceStateEnum.ACTIVE)
+  //     promises.push(computeClient.instance.close(instance.id));
+  // });
+
+  // await Promise.all(promises);
+
+  const clusterInstance: ClusterInstance =
+    await computeClient.instance.get(clusterInstancesExtended[0].id);
+
+  const instanceDomains: Domain[] = await computeClient.instance.getDomains(
+    clusterInstance.id
+  );
+
+  const domainUpdate: Domain = await computeClient.instance.updateDomain(
+    clusterInstance.id,
+    instanceDomains[0].id,
+    {
+      name: "test2.com",
+      link: "provider.palmito.duckdns.org:31494",
+      type: DomainTypeEnum.DOMAIN,
+    }
+  );
+
+  await computeClient.instance.deleteDomain(
+    clusterInstance.id,
+    instanceDomains[0].id
+  );
+
+  const domains: Domain[] =
+  await computeClient.instance.getDomains(
+    clusterInstance.id
+  );
+
+  const response: { order: ClusterInstanceOrder; liveLogs: string[] } =
+    await computeClient.instance.getClusterInstanceOrder(
+      clusterInstance.activeOrder
+    );
+
+  const logs = await computeClient.instance.getClusterInstanceOrderLogs(
+    response.order.id,
+    { from: 0, to: 1000, logType: InstanceLogType.INSTANCE_LOGS }
+  );
+
+  await computeClient.instance.update("63612a842e50ee3ffbebcf06", clusterInstance.id, updateInstance)
+
+  // await Promise.all(
+  //   clusterInstancesExtended.map((instance) => {
+  //     if (instance.state === ClusterInstanceStateEnum.ACTIVE)
+  //       computeClient.instance.close(instance.id);
+  //   })
+  // );
+
+  // const promises: any[] = [];
+  // const clusterInstances: ExtendedClusterInstance[] =
+  //   await computeClient.cluster.getInstances(clusters[0]._id, {
+  //     skip: 0,
+  //     limit: 10,
+  //   });
   // clusterInstances.forEach((instance) => {
   //   if (instance.state === ClusterInstanceStateEnum.ACTIVE)
   //     promises.push(computeClient.clusterInstance.close(instance._id));
   // });
 
   // await Promise.all(promises);
-
-  // const clusterInstance: ClusterInstance =
-  //   await computeClient.clusterInstance.get(clusterInstances[0]._id);
-
-  // const domains: Domain[] = await computeClient.clusterInstance.getDomains(
-  //   clusterInstance._id
-  // );
-
-  // const domainUpdate: Domain = await computeClient.clusterInstance.updateDomain(
-  //   clusterInstance._id,
-  //   domains[0]._id,
-  //   {~
-  //     name: "test2.com",
-  //     link: "provider.palmito.duckdns.org:31494",
-  //     type: DomainTypeEnum.DOMAIN,
-  //   }
-  // );
-
-  // await computeClient.clusterInstance.deleteDomain(
-  //   clusterInstance._id,
-  //   domains[0]._id
-  // );
-
-  // const domains: Domain[] =
-  // await computeClient.clusterInstance.getClusterInstanceDomains(
-  //   clusterInstance._id
-  // );
-
-  // const response: { order: ClusterInstanceOrder; liveLogs: string[] } =
-  //   await computeClient.clusterInstance.getClusterInstanceOrder(
-  //     clusterInstance.activeOrder
-  //   );
-
-  // const logs = await computeClient.clusterInstance.getClusterInstanceOrderLogs(
-  //   response.order._id,
-  //   { from: 0, to: 1000, logType: InstanceLogType.INSTANCE_LOGS }
-  // );
-
-  // await computeClient.clusterInstance.updateClusterInstance(updateInstance)
-
-  // await Promise.all(
-  //   clusterInstances.map((instance) => {
-  //     if (instance.state === ClusterInstanceStateEnum.ACTIVE)
-  //       computeClient.clusterInstance.closeClusterInstance(instance._id);
-  //   })
-  // );
 
   Logger.info("asdasd");
 })();
